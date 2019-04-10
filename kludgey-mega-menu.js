506290d1ae8369bf25887d8bfb0b9fbc66c8dd29
AlmeroSteyn <almero.steyn@gmail.com>
2019-04-10 11:31:17 +0200

    Mega menu functionality

--- kludgey-mega-menu.js
+++ kludgey-mega-menu.js
@@ -1,41 +1,93 @@
-$(document).ready(function () {
+$(function() {
+  let menuParents = $('nav > ul > li > a');
+  let submenus = $('nav ul ul');
 
-    let menuParents = $('nav ul li a');
-    let submenus = $('nav ul ul');
+  /**
+   * Handles Shift+Tab events and opens previous submenu if available
+   */
+  menuParents.on('keydown', function(e) {
+    if (
+      e.shiftKey &&
+      ((e.key && e.key === 'Tab') || (e.keyCode && e.keyCode === 9))
+    ) {
+      e.preventDefault();
 
-    /**
-     * Handle the keyboard accessibility side of things
-     */
-    menuParents.focus(function () {
+      let subMenu = $(this).next('ul');
+      let prevMenu = $(this)
+        .closest('li')
+        .prev('li');
+      let prevSubMenu = $(this)
+        .closest('li')
+        .prev('li')
+        .find('ul');
 
-        submenus.hide();
-        let subMenu = $(this).next('ul');
-
-        if (subMenu.length > 0) {
-            subMenu.show();
-
-            let subMenuLinks = subMenu.find('a[href]');
-
-            subMenuLinks.last().blur(function () {
-                subMenu.hide();
-            });
-
-        }
-    });
-
-    /**
-     *
-     * Handle hover
-     */
-    menuParents.hover(function () {
-        let subMenu = $(this).next('ul');
-
-        subMenu.show();
-
-    }, function () {
-        let subMenu = $(this).next('ul');
-
+      if (subMenu.length > 0) {
         subMenu.hide();
-    });
+      }
 
+      if (prevSubMenu.length > 0) {
+        prevSubMenu.show();
+        prevSubMenu
+          .find('a[href]')
+          .last()
+          .trigger('focus');
+      } else {
+        prevMenu.find('a').trigger('focus');
+      }
+    }
+  });
+
+  /**
+   * Handle forward focus keyboard events
+   */
+  menuParents.on('focus', function() {
+    submenus.hide();
+
+    let subMenu = $(this).next('ul');
+
+    if (subMenu.length > 0) {
+      subMenu.show();
+
+      let subMenuLinks = subMenu.find('a[href]');
+
+      let keyDownHandler = function(e) {
+        if (
+          !e.shiftKey &&
+          ((e.key && e.key === 'Tab') || (e.keyCode && e.keyCode === 9))
+        ) {
+          subMenu.hide();
+        }
+      };
+
+      subMenuLinks.last().on('keydown', keyDownHandler);
+    }
+  });
+
+  /**
+   * Handles mouse hover events
+   */
+  menuParents
+    .on('mouseenter', function() {
+      submenus.hide();
+
+      let subMenu = $(this).next('ul');
+
+      subMenu.show();
+    })
+    .on('mouseleave', function() {
+      let subMenu = $(this).next('ul');
+
+      let mouseEnterHandler = function() {
+        $(this).show();
+      };
+
+      let mouseLeaveHandler = function() {
+        $(this).hide();
+      };
+
+      subMenu.on('mouseenter', mouseEnterHandler);
+      subMenu.on('mouseleave', mouseLeaveHandler);
+
+      subMenu.hide();
+    });
 });
